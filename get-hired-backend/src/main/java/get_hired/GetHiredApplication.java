61e5ef55eccf8118c12eb20cbba1636a2b6fab64
shiv6393 <shivampandey02080@gmail.com>
2026-01-30 17:29:36 +0530

    Backend Configured

---
+++ get-hired-backend/src/main/java/get_hired/GetHiredApplication.java
@@ -0,0 +1,13 @@
+package get_hired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetHiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetHiredApplication.class, args);
+	}
+
+}
---
+++ get-hired-backend/src/test/java/get_hired/GetHiredApplicationTests.java
@@ -0,0 +1,13 @@
+package get_hired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetHiredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
